687fa1ac560d1c208ed20ffb4786138d7b16f527
Casey de la Vega-Rawson <spamcasey@gmail.com>
2018-03-21 20:14:11 -0700

    add Date type converter

--- app/src/main/java/caseydlvr/recurringtasks/db/TaskDatabase.java
+++ app/src/main/java/caseydlvr/recurringtasks/db/TaskDatabase.java
@@ -2,8 +2,10 @@ package caseydlvr.recurringtasks.db;
 
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverters;
 
 @Database(entities = {Task.class}, version = 1)
+@TypeConverters({Converters.class})
 public abstract class TaskDatabase extends RoomDatabase {
     public abstract TaskDao taskDao();
 }
---
+++ app/src/main/java/caseydlvr/recurringtasks/ui/TaskFragment.java
@@ -0,0 +1,7 @@
+package caseydlvr.recurringtasks.ui;
+
+import android.support.v4.app.Fragment;
+
+
+public class TaskFragment extends Fragment {
+}
