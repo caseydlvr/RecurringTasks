44027a8a1c73a32db8c7201c7d1cec6dd9146b72
Casey de la Vega-Rawson <spamcasey@gmail.com>
2018-03-23 21:55:06 -0700

    make AppDatabase a singleton

--- app/src/main/java/caseydlvr/recurringtasks/db/AppDatabase.java
+++ app/src/main/java/caseydlvr/recurringtasks/db/AppDatabase.java
@@ -1,11 +1,29 @@
 package caseydlvr.recurringtasks.db;
 
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.arch.persistence.room.TypeConverters;
+import android.content.Context;
 
 @Database(entities = {Task.class}, version = 1)
 @TypeConverters({Converters.class})
 public abstract class AppDatabase extends RoomDatabase {
+
+    private static AppDatabase INSTANCE;
+
     public abstract TaskDao taskDao();
+
+    public static AppDatabase getAppDatabase(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE = Room
+                    .databaseBuilder(
+                            context.getApplicationContext(),
+                            AppDatabase.class,
+                            "task-database")
+                    .build();
+        }
+
+        return INSTANCE;
+    }
 }
