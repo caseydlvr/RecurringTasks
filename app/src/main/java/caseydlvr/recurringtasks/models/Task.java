1614e3fe132aa6fc70a371860400fa6eb39dd0e7
Casey de la Vega-Rawson <spamcasey@gmail.com>
2018-03-25 20:56:52 -0700

    removed unused import

--- app/src/main/java/caseydlvr/recurringtasks/models/Task.java
+++ app/src/main/java/caseydlvr/recurringtasks/models/Task.java
@@ -2,7 +2,6 @@ package caseydlvr.recurringtasks.models;
 
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
-import android.arch.persistence.room.Ignore;
 import android.arch.persistence.room.PrimaryKey;
 
 import java.util.Date;
