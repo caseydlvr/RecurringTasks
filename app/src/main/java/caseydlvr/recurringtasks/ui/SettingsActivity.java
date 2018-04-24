22dd43b4aafceb7f67e044ea16989a4639755097
Casey de la Vega-Rawson <spamcasey@gmail.com>
2018-04-23 18:44:00 -0700

    add settings page with notification setting

---
+++ app/src/main/java/caseydlvr/recurringtasks/ui/SettingsActivity.java
@@ -0,0 +1,23 @@
+package caseydlvr.recurringtasks.ui;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+    public static final String KEY_SHOW_NOTIFICATIONS = "pref_show_notifications";
+
+    private SharedPreferences mSharedPreferences;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
+}
