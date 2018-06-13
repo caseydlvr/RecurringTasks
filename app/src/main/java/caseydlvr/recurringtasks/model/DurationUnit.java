a031099b31f98edc233b8ecf111736717e7fd196
Casey de la Vega-Rawson <spamcasey@gmail.com>
2018-06-12 22:45:33 -0700

    comments for DurationUnit

--- app/src/main/java/caseydlvr/recurringtasks/model/DurationUnit.java
+++ app/src/main/java/caseydlvr/recurringtasks/model/DurationUnit.java
@@ -7,6 +7,15 @@ import java.util.List;
 
 import caseydlvr.recurringtasks.R;
 
+/**
+ * Represents a unit of time duration, such as 'day', 'week'. Includes a key string, as well as
+ * display strings for both singular and plural versions of the duration. The key string is a
+ * constant, while the display strings are localized (and therefore dynamic).
+ *
+ * Includes static convenience methods for building individual DurationUnits as well as Collections
+ * of DurationUnits. These static methods require a Context in order to fetch the display names from
+ * Resources (allowing localization of the display names).
+ */
 public class DurationUnit {
     public static final String KEY_DAY = "day";
     public static final String KEY_WEEK = "week";
@@ -28,14 +37,23 @@ public class DurationUnit {
         mNameSingular = nameSingular;
     }
 
+    /**
+     * @return KEY_ String constant for this DurationUnit
+     */
     public String getKey() {
         return mKey;
     }
 
+    /**
+     * @return Localized plural display String for this DurationUnit. For displaying the unit in the UI.
+     */
     public String getName() {
         return mName;
     }
 
+    /**
+     * @return Localized singular display String for this DurationUnit. For displaying the unit in the UI.
+     */
     public String getNameSingular() {
         return mNameSingular;
     }
@@ -45,6 +63,10 @@ public class DurationUnit {
         return mName;
     }
 
+    /**
+     * @param context Context to use for fetching String resources
+     * @return        List containing all DurationUnits
+     */
     public static List<DurationUnit> buildList(Context context) {
         ArrayList<DurationUnit> durationUnits = new ArrayList<>(COUNT);
         durationUnits.add(INDEX_DAY, buildDay(context));
@@ -55,6 +77,14 @@ public class DurationUnit {
         return durationUnits;
     }
 
+    /**
+     * Builds a DurationUnit for the given key. Uses String resources to set mName and mNameSingular
+     * so that these display Strings are localized.
+     *
+     * @param context Context to use for fetching String resources
+     * @param key     KEY_ constant of the DurationUnit to build
+     * @return        DurationUnit for the given key
+     */
     public static DurationUnit build(Context context, String key) {
         DurationUnit durationUnit;
 
@@ -78,6 +108,18 @@ public class DurationUnit {
         return durationUnit;
     }
 
+    /**
+     * Gets the Index of the DurationUnit specified by key in the List returned by buildList(Context).
+     *
+     * An example of when this is useful: buildList is used to populate a spinner and a list index
+     * must be specified to set which DurationUnit in the spinner should be selected, but the user
+     * only has a key available. Getting the index using getIndex saves the user from searching
+     * the list for a DurationUnit with a matching key just to determine the appropriate index.
+     *
+     * @param key KEY_ constant
+     * @return    Index of the DurationUnit in the List returned by buildList(Context)
+     * @see       #buildList(Context)
+     */
     public static int getIndex(String key) {
         int index;
 
