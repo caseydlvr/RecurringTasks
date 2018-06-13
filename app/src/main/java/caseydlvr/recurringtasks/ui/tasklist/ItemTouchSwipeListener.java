def04f990f52de88710561194cb8860b7652b914
Casey de la Vega-Rawson <spamcasey@gmail.com>
2018-06-12 23:49:01 -0700

    comments for ItemTouchSwipeTarget

--- app/src/main/java/caseydlvr/recurringtasks/ui/tasklist/ItemTouchSwipeListener.java
+++ app/src/main/java/caseydlvr/recurringtasks/ui/tasklist/ItemTouchSwipeListener.java
@@ -3,14 +3,16 @@ package caseydlvr.recurringtasks.ui.tasklist;
 import android.support.v7.widget.RecyclerView;
 
 /**
- * Interface for a RecyclerView swipe to dismiss listener.
+ * Interface for a RecyclerView swipe to dismiss listener. Implemented by classes that wish to
+ * respond to swipe to dismiss actions.
  */
 public interface ItemTouchSwipeListener {
     /**
      * Callback triggered when the user swipes away a RecyclerView item
      *
      * @param viewHolder ViewHolder swiped by the user
-     * @param direction  Direction of the swipe
+     * @param direction  Direction of the swipe. Corresponds to an ItemTouchHelper direction constant
+     * @see              android.support.v7.widget.helper.ItemTouchHelper
      */
     void onItemSwiped(RecyclerView.ViewHolder viewHolder, int direction);
 }
--- app/src/main/java/caseydlvr/recurringtasks/ui/tasklist/ItemTouchSwipeTarget.java
+++ app/src/main/java/caseydlvr/recurringtasks/ui/tasklist/ItemTouchSwipeTarget.java
@@ -2,7 +2,20 @@ package caseydlvr.recurringtasks.ui.tasklist;
 
 import android.view.View;
 
+/**
+ * Interface for a RecyclerView swipe to dismiss target. Implemented by ViewHolders that can be
+ * swiped to dismiss.
+ */
 public interface ItemTouchSwipeTarget {
+    /**
+     * @return View to show in the foreground while a swipe animation is in progress. This is
+     *         typically the View that is being swiped away
+     */
     View getSwipeForeground();
+
+    /**
+     * @param direction direction of the swipe. Corresponds to an ItemTouchHelper direction constant
+     * @see             android.support.v7.widget.helper.ItemTouchHelper
+     */
     void prepareSwipeBackground(int direction);
 }
