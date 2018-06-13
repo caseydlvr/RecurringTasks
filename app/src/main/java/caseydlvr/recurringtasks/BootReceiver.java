8a0efde1a86688713a2f2428b2d57e1e7fde6e08
Casey de la Vega-Rawson <spamcasey@gmail.com>
2018-06-12 20:04:46 -0700

    comments for BootReceiver

--- app/src/main/java/caseydlvr/recurringtasks/BootReceiver.java
+++ app/src/main/java/caseydlvr/recurringtasks/BootReceiver.java
@@ -5,6 +5,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+/**
+ * BroadcastReceiver for the BOOT_COMPLETED system action. This receiver doesn't run any of its own
+ * code, but it being triggered causes the Application's onCreate() to run, which does the
+ * necessary initialization that is required for the app to function properly after a reboot.
+ */
 public class BootReceiver extends BroadcastReceiver {
 
     private static final String TAG = BootReceiver.class.getSimpleName();
