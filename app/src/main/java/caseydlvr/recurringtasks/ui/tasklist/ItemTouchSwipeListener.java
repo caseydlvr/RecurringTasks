e85ae3e0c657dc446b1fe59cb8f30c8256bcebee
Casey de la Vega-Rawson <spamcasey@gmail.com>
2018-06-12 23:39:07 -0700

    comments for ItemTouchSwipeListener

--- app/src/main/java/caseydlvr/recurringtasks/ui/tasklist/ItemTouchSwipeListener.java
+++ app/src/main/java/caseydlvr/recurringtasks/ui/tasklist/ItemTouchSwipeListener.java
@@ -2,6 +2,15 @@ package caseydlvr.recurringtasks.ui.tasklist;
 
 import android.support.v7.widget.RecyclerView;
 
+/**
+ * Interface for a RecyclerView swipe to dismiss listener.
+ */
 public interface ItemTouchSwipeListener {
+    /**
+     * Callback triggered when the user swipes away a RecyclerView item
+     *
+     * @param viewHolder ViewHolder swiped by the user
+     * @param direction  Direction of the swipe
+     */
     void onItemSwiped(RecyclerView.ViewHolder viewHolder, int direction);
 }
