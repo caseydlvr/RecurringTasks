868955636c04beed628be71c3e68a707b41c179b
Casey de la Vega-Rawson <spamcasey@gmail.com>
2018-06-19 17:06:29 -0700

    dynamically set default value of max notifications preference

--- app/src/main/java/caseydlvr/recurringtasks/notifications/NotificationUtils.java
+++ app/src/main/java/caseydlvr/recurringtasks/notifications/NotificationUtils.java
@@ -3,11 +3,17 @@ package caseydlvr.recurringtasks.notifications;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
 /**
  * Static functions for handling notification related actions
  */
 public class NotificationUtils {
+
+    private static final String TAG = NotificationUtils.class.getSimpleName();
+
+    private static final int DEFAULT_MAX_NOTIFICATIONS = 4;
+    private static final int DEFAULT_MAX_NOTIFICATIONS_PRE_N = 2;
 
     /**
      * Dismisses the notification with the provided notification ID. For a task notification, the
@@ -38,5 +44,21 @@ public class NotificationUtils {
         Intent intent = new Intent(context, NotificationReceiver.class);
         intent.setAction(NotificationReceiver.ACTION_SEND_NOTIFICATIONS);
         context.sendBroadcast(intent);
+    }
+
+    /**
+     * Max number of notifications to show, by default. This depends on Android version. Since
+     * grouping on Pre-N devices reduces notification functionality, a lower max is used so the
+     * default max doesn't cause grouping. Since grouping has minimal downsides on N+ devices, a
+     * higher default max is used.
+     *
+     * @return max number of notifications to show, by default
+     */
+    public static int getDefaultMaxNotifications() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            return DEFAULT_MAX_NOTIFICATIONS_PRE_N;
+        } else {
+            return DEFAULT_MAX_NOTIFICATIONS;
+        }
     }
 }
--- app/src/main/java/caseydlvr/recurringtasks/ui/settings/SettingsFragment.java
+++ app/src/main/java/caseydlvr/recurringtasks/ui/settings/SettingsFragment.java
@@ -1,10 +1,12 @@
 package caseydlvr.recurringtasks.ui.settings;
 
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
 
 import caseydlvr.recurringtasks.R;
+import caseydlvr.recurringtasks.notifications.NotificationUtils;
 
 public class SettingsFragment extends PreferenceFragment {
     @Override
@@ -12,5 +14,19 @@ public class SettingsFragment extends PreferenceFragment {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.preferences);
+        setMaxNotificationsDefaultValue();
+    }
+
+    /**
+     * Sets a default value for max notifications if no value is currently set. Necessary because
+     * default value for this preference is dynamic, dependent on Android version. Defaults and
+     * dynamic logic are therefore maintained in NotificationUtils, rather than set as a static
+     * value in preferences.xml.
+     */
+    private void setMaxNotificationsDefaultValue() {
+        ListPreference maxNotifications = (ListPreference) findPreference(SettingsActivity.KEY_MAX_NOTIFICATIONS);
+        if (maxNotifications.getValue() == null) {
+            maxNotifications.setValue(String.valueOf(NotificationUtils.getDefaultMaxNotifications()));
+        }
     }
 }
