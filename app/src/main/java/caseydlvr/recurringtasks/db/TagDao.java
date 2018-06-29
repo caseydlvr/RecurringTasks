1e2312f4b2eebf6afcf93f32290b447e5298b05c
Casey de la Vega-Rawson <spamcasey@gmail.com>
2018-06-28 21:31:22 -0700

    tagDao missing @Dao

--- app/src/main/java/caseydlvr/recurringtasks/db/TagDao.java
+++ app/src/main/java/caseydlvr/recurringtasks/db/TagDao.java
@@ -1,6 +1,7 @@
 package caseydlvr.recurringtasks.db;
 
 import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
@@ -12,13 +13,14 @@ import caseydlvr.recurringtasks.model.Tag;
 
 import static android.arch.persistence.room.OnConflictStrategy.REPLACE;
 
+@Dao
 public interface TagDao {
 
     @Query("Select * FROM tags")
     LiveData<List<Tag>> getAllTags();
 
     @Insert(onConflict = REPLACE)
-    int insert(Tag tag);
+    long insert(Tag tag);
 
     @Update
     void update(Tag tag);
