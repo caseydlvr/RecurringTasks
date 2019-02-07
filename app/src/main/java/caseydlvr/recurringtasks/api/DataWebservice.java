a578872e9d7d5e0553391795bb23a1bad6077a8e
Casey de la Vega-Rawson <spamcasey@gmail.com>
2019-02-07 15:53:39 -0800

    add Retrofit API interface for the REST server

---
+++ app/src/main/java/caseydlvr/recurringtasks/api/DataWebservice.java
@@ -0,0 +1,45 @@
+package caseydlvr.recurringtasks.api;
+
+import java.util.List;
+
+import caseydlvr.recurringtasks.model.Tag;
+import caseydlvr.recurringtasks.model.Task;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.PATCH;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface DataWebservice {
+    @GET("/tasks")
+    Call<List<Task>> getTasks();
+
+    @GET("/tasks/{id}")
+    Call<Task> getTask(@Path("id") long taskId);
+
+    @POST("/tasks")
+    Call<Task> createTask(@Body Task task);
+
+    @POST("/tasks/{id}/complete")
+    Call<Task> completeTask(@Path("id") long taskId);
+
+    @PATCH("/tasks/{id}")
+    Call<Task> updateTask(@Path("id") long taskId, @Body Task task);
+
+    @DELETE("/tasks/{id}")
+    Call<Void> deleteTask(@Path("id") long taskId);
+
+    @GET("/tags")
+    Call<List<Tag>> getTags();
+
+    @POST("/tags")
+    Call<Tag> createTag(@Body Tag tag);
+
+    @PATCH("/tags/{id}")
+    Call<Tag> updateTag(@Path("id") int tagId, @Body Tag tag);
+
+    @DELETE("/tags/{id}")
+    Call<Void> deleteTag(@Path("id") int tagId);
+}
