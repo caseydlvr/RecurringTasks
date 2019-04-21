8b636a9d70b6bc495c48ef22cc701fb71e099fdf
Casey de la Vega-Rawson <spamcasey@gmail.com>
2019-04-20 17:05:49 -0700

    add JsonSerializer to LocalDateTypeAdapter

--- app/src/main/java/caseydlvr/recurringtasks/api/LocalDateTypeAdapter.java
+++ app/src/main/java/caseydlvr/recurringtasks/api/LocalDateTypeAdapter.java
@@ -4,19 +4,31 @@ import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
 
 import org.threeten.bp.LocalDate;
 import org.threeten.bp.format.DateTimeFormatter;
 
 import java.lang.reflect.Type;
 
-public class LocalDateTypeAdapter implements JsonDeserializer {
-    private static final DateTimeFormatter SERVER_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+public class LocalDateTypeAdapter implements JsonDeserializer, JsonSerializer {
+    private static final DateTimeFormatter RX_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+    private static final DateTimeFormatter TX_FORMAT = DateTimeFormatter.ISO_LOCAL_DATE;
 
     @Override
     public Object deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
         String dateString = json.getAsString();
 
-        return dateString == null ? null: LocalDate.parse(dateString, SERVER_FORMAT);
+        return dateString == null ? null: LocalDate.parse(dateString, RX_FORMAT);
+    }
+
+    @Override
+    public JsonElement serialize(Object src, Type typeOfSrc, JsonSerializationContext context) {
+        LocalDate localDate = (LocalDate) src;
+        String dateString = localDate == null ? "" : localDate.format(TX_FORMAT);
+
+        return new JsonPrimitive(dateString);
     }
 }
