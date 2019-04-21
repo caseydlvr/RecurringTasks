00f99616d5e8456c46be1e54dae4d4ea73050ec2
Casey de la Vega-Rawson <spamcasey@gmail.com>
2019-04-20 17:38:52 -0700

    add TaskWithTags model

--- app/src/main/java/caseydlvr/recurringtasks/model/TaskWithTags.java
+++ app/src/main/java/caseydlvr/recurringtasks/model/TaskWithTags.java
@@ -1,50 +1,25 @@
 package caseydlvr.recurringtasks.model;
 
-import java.util.Comparator;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 import java.util.List;
 
-import androidx.annotation.Nullable;
-import androidx.room.Embedded;
-import androidx.room.Relation;
+public class TaskWithTags extends Task {
 
-public class TaskWithTags {
-    @Embedded
-    private Task mTask;
-
-    @Relation(parentColumn = "id", entityColumn = "task_id", entity = TaskTag.class, projection = "tag_id")
-    private List<Integer> mTagIds;
-
-    public Task getTask() {
-        return mTask;
+    public TaskWithTags() {
+        super();
     }
 
-    public void setTask(Task task) {
-        mTask = task;
+    @SerializedName("tags")
+    @Expose
+    private List<Tag> mTags;
+
+    public List<Tag> getTags() {
+        return mTags;
     }
 
-    public List<Integer> getTagIds() {
-        return mTagIds;
-    }
-
-    public void setTagIds(List<Integer> tagIds) {
-        mTagIds = tagIds;
-    }
-
-    @Override
-    public boolean equals(@Nullable Object obj) {
-        if (obj == null) return false;
-        if (!TaskWithTags.class.isAssignableFrom(obj.getClass())) return false;
-
-        final TaskWithTags other = (TaskWithTags) obj;
-
-        return (other.getTask().equals(mTask) && other.getTagIds().equals(mTagIds));
-    }
-
-    public static class TaskWithTagsComparator implements Comparator<TaskWithTags> {
-
-        @Override
-        public int compare(TaskWithTags o1, TaskWithTags o2) {
-            return new Task.TaskComparator().compare(o1.getTask(), o2.getTask());
-        }
+    public void setTags(List<Tag> tags) {
+        mTags = tags;
     }
 }
